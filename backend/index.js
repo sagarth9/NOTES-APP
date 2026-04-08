30fb078017ac8f13db977b1aa497bf4a7915b46c
sagar <sagarthakur998449@gmail.com>
2026-04-08 18:54:42 +0530

    Change server port to use environment variable PORT

--- backend/index.js
+++ backend/index.js
@@ -316,6 +316,8 @@ app.get("/search-notes", authenticateToken, async (req, res) => {
     }
 });
 
-app.listen(8000);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
